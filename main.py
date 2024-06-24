f2db5c3d85b88e5792a774b3f9c28cb72f38b549
vish <vish0290@gmail.com>
2024-06-24 12:13:17 +0200

    minor update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ templates = Jinja2Templates(directory="app/templates")
 app.include_router(router= users.router)
 
 
-product = [{'product':'iphone 13', 'price':'50000'},{'product':'iphone 14','price':'60000'}]
+product = [{'product':'iphone 15', 'price':'50000'},{'product':'iphone 14','price':'60000'}]
 @app.get("/",response_class=HTMLResponse)
 def homepage(request: Request):
     return templates.TemplateResponse("home.html",{"request":request, 'product_list':product})
